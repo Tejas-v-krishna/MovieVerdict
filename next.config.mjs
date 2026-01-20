86efaf56e96df3b0016711cdf81675777598d11a
Tejas-v-krishna <tejaskrishna2018@gmail.com>
2026-01-20 21:15:24 +0530

    Install next-intl and basic setup

---
+++ next.config.mjs
@@ -0,0 +1,23 @@
+import createNextIntlPlugin from 'next-intl/plugin';
+
+const withNextIntl = createNextIntlPlugin(
+    './src/i18n/request.ts'
+);
+
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+    images: {
+        remotePatterns: [
+            {
+                protocol: "https",
+                hostname: "lh3.googleusercontent.com",
+            },
+            {
+                protocol: "https",
+                hostname: "image.tmdb.org",
+            },
+        ],
+    },
+};
+
+export default withNextIntl(nextConfig);
